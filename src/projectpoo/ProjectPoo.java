2aaf5cf3b33a7ab18869e55ee35915d04acb29f9
JuanPablo22001 <55415193+JuanPablo22001@users.noreply.github.com>
2021-08-25 17:02:05 -0500

    Pruebaa 2

--- src/projectpoo/ProjectPoo.java
+++ src/projectpoo/ProjectPoo.java
@@ -15,7 +15,7 @@ public class ProjectPoo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        //actualizacion e.e 
     }
     
 }
